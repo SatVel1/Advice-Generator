a9a22190e9bb16d536b9c2644b50790aaf2a6366
Sat <sat@Sats-MacBook-Pro.local>
2022-03-30 10:11:10 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const[myAdvice, state] = useState([])
+    const[prevAdvice, prevState] = useState([''])
+    const [initial, firstNum] = useState([0])
+
+    //   Executes at the render of the component
+    useEffect(() => {
+        fetchAdvice();
+    }, []);
+
+    const fetchAdvice = () => {
+        axios.get('https://api.adviceslip.com/advice')
+            .then((response) => {
+                const { advice } = response.data.slip
+
+                if(initial == 0){
+                    prevState(advice);
+                    firstNum(1);
+                }
+
+                if (prevAdvice == advice){
+                    fetchAdvice();
+                    console.log("Advice" + advice);
+                    console.log("Prev Advice" + prevAdvice);
+                }
+
+                state(advice);
+                prevState(advice);
+
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    return (
+        <div className='app'>
+            <div className='card'>
+                <h1 className='heading'>{myAdvice}</h1>
+                <button className="glow-on-hover" type="button" onClick={fetchAdvice}>GENERATE ADVICE</button>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
